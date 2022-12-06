e52033d84040846c71ce74ce2d0fa6166aa3179b
b21946146 <deniz.gonencx@gmail.com>
2022-12-06 19:46:14 +0300

    Unlabeled Star Wars Script

--- sentimentalanalysis/src/main.py
+++ sentimentalanalysis/src/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import glob
-import os
 
-df = pd.read_fwf("/../data/neg/0_3.txt")
-print(df.head())
+# read script and transform to DataFrame
+df = pd.read_csv("../data/raw/starwars/SW_EpisodeIV.txt", sep='" "', engine='python')
+df.drop(['"index'], axis=1, inplace=True)
+df.columns = ["character", "dialogue"]
+df["dialogue"] = df["dialogue"].apply(lambda x: x[:-1])
+
