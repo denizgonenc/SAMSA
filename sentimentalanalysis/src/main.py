c5bd9d96158a3bd7dd192bd14b15ab95a9545f5d
b21946146 <deniz.gonencx@gmail.com>
2022-12-21 16:14:24 +0300

    Used NRC to calculate the overall basic sentiment scores of script

--- sentimentalanalysis/src/main.py
+++ sentimentalanalysis/src/main.py
@@ -12,23 +12,27 @@ df["dialogue"] = df["dialogue"].apply(lambda x: x[:-1])
 
 print(df.head())
 
-vectorizer = CountVectorizer(stop_words='english')
-cv_transform = vectorizer.fit_transform(df["dialogue"])
-words_vector = vectorizer.get_feature_names_out()
-freq_vector = np.asarray(cv_transform.sum(axis=0))
+cv_vectorizer = CountVectorizer(stop_words='english')
+cv_transform = cv_vectorizer.fit_transform(df["dialogue"])
+words_vector = cv_vectorizer.get_feature_names_out()
+freq_vector = np.asarray(cv_transform.sum(axis=0)).flatten()
 
-print(words_vector)
-print(freq_vector)
+words_freq = pd.DataFrame({'word': words_vector, 'frequency': freq_vector}, columns=['word', 'frequency'])
+print(words_freq)
 
 nrc = pd.read_csv("../data/lexicon/NRC-Emotion-Lexicon-Wordlevel.txt", sep='\t', engine='python')
 nrc.columns = ["word", "sentiment", "value"]
+nrc = nrc[nrc['value'] == 1].drop(['value'], axis=1)
+nrc.reset_index(inplace=True, drop=True)
 
-print(nrc.tail())
+print(nrc.head())
 
+df_combined = pd.merge(words_freq, nrc, how='inner', on='word')
+print(df_combined)
 
-
-
-
+sentiments = ["positive", "negative", "anger", "anticipation", "disgust", "fear", "joy", "sadness", "surprise", "trust"]
+for s in sentiments:
+    print("{} - {}".format(s, df_combined[df_combined['sentiment'] == s]['frequency'].sum()))
 
 # TODO need lemmatizer e.g. attack -> attacks
 # TODO = profit ??
