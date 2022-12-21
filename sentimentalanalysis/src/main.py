30876af89fe2bb7d3b47d9c326f1271c9c460f91
b21946146 <deniz.gonencx@gmail.com>
2022-12-21 14:06:44 +0300

    Unlabeled Star Wars Script

--- sentimentalanalysis/src/main.py
+++ sentimentalanalysis/src/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 # read script and transform to DataFrame
 df = pd.read_csv("../data/raw/starwars/SW_EpisodeIV.txt", sep='" "', engine='python')
@@ -6,3 +8,10 @@ df.drop(['"index'], axis=1, inplace=True)
 df.columns = ["character", "dialogue"]
 df["dialogue"] = df["dialogue"].apply(lambda x: x[:-1])
 
+
+print(df.head())
+
+
+# TODO NRE Lexicon -> labeling script
+# Vectorizing script ?
+# = profit ??
