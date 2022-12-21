8a96adb6945ec2fba920feab53ab767923384364
b21946146 <deniz.gonencx@gmail.com>
2022-12-21 15:31:02 +0300

    Added NRC to the directories, vectorizer

--- sentimentalanalysis/src/main.py
+++ sentimentalanalysis/src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # read script and transform to DataFrame
 df = pd.read_csv("../data/raw/starwars/SW_EpisodeIV.txt", sep='" "', engine='python')
@@ -11,7 +12,17 @@ df["dialogue"] = df["dialogue"].apply(lambda x: x[:-1])
 
 print(df.head())
 
+vectorizer = CountVectorizer(stop_words='english')
+cv_transform = vectorizer.fit_transform(df["dialogue"])
+words_vector = vectorizer.get_feature_names_out()
+freq_vector = np.asarray(cv_transform.sum(axis=0))
 
-# TODO NRE Lexicon -> labeling script
-# Vectorizing script ?
-# = profit ??
+print(words_vector)
+print(freq_vector)
+
+
+
+
+
+# TODO need lemmatizer e.g. attack -> attacks
+# TODO = profit ??
