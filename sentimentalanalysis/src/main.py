1f1b3c73f316d51a35bdf1e9dab90d1f903f2bb4
b21946146 <deniz.gonencx@gmail.com>
2023-04-07 00:18:45 +0300

    anger and anticipation

--- sentimentalanalysis/src/main.py
+++ sentimentalanalysis/src/main.py
@@ -1,10 +1,17 @@
 import pickle
 
+sentiments = ["positive", "negative", "anger", "anticipation", "disgust", "fear", "joy", "sadness", "surprise", "trust"]
+sentiment_scores = {"positive": {}, "negative": {}, "anger": {}, "anticipation": {}, "disgust": {},
+                    "fear": {}, "joy": {}, "sadness": {}, "surprise": {}, "trust": {}}
 # Read scores from pickle
-with open('../data/processed/negative.pkl', 'rb') as f:
-    negative = dict(pickle.load(f))
+for s in sentiments:
+    with open('../data/processed/sentiments/' + s + '.pkl', 'rb') as f:
+        print(s)
+        temp = dict(pickle.load(f))
+        sentiment_scores[s] = temp
 
-print(negative)
+print(sentiment_scores)
 
 
-
+with open("../data/processed/sentiments.pkl", "wb") as f:
+    pickle.dump(sentiment_scores, f)
