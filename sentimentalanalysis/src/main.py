d0f334bc9adb628a3040a0042858e791d7d1f539
b21946146 <deniz.gonencx@gmail.com>
2023-05-03 10:18:30 +0300

    prediction of sentence

--- sentimentalanalysis/src/main.py
+++ sentimentalanalysis/src/main.py
@@ -1,6 +1,27 @@
 import pickle
+import nltk
+import string
 
 with open('../data/processed/sentiments.pkl', 'rb') as f:
     sentiment_scores = pickle.load(f)
 
-print(sentiment_scores["fear"])
+sentiments = ["anger", "anticipation", "disgust", "fear", "joy", "sadness", "surprise", "trust"]
+
+def predict(sentence):
+    words = [word for word in nltk.word_tokenize(sentence) if word not in string.punctuation]
+
+    scores = []
+    for s in sentiments:
+        score = 0
+        count = 0
+        for w in words:
+            try:
+                score += sentiment_scores[s][w]
+                count += 1
+            except Exception:
+                continue
+        score /= count
+        scores.append(score)
+    return sentiments[scores.index(max(scores))], max(scores)
+
+print(predict("I hate you"))
